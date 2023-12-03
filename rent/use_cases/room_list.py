98e84ca6f8041d840af1181835e581ba60672ed4
Borohoul Soguelni Malachie <malachieborohoul@gmail.com>
2023-12-03 22:54:24 +0100

    Token Authentification

--- rent/use_cases/room_list.py
+++ rent/use_cases/room_list.py
@@ -1,5 +1,5 @@
 
-# ResponseSuccessResponseSuccessResponseSu
+# ResponseSuccessResponseSuccessResponseS
 from rent.responses import ResponseSuccess
 def room_list_use_case(repo, request):
     rooms= repo.list()
