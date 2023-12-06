253c7e55a5152dde56121256140ce9aa7dd09be4
Borohoul Soguelni Malachie <malachieborohoul@gmail.com>
2023-12-06 23:05:24 +0100

    Views

--- rent/use_cases/room_list.py
+++ rent/use_cases/room_list.py
@@ -1,5 +1,6 @@
 
-#  ResponseSuccessResponseSuccessResp
+#  ResponseSuccessResponseSuccessRes
+
 from rent.responses import ResponseSuccess
 def room_list_use_case(repo, request):
     rooms= repo.list()
